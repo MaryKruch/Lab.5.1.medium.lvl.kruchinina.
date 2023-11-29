4b55f015097a0359ba4955e57c9170ec1cac0345
1 <1@DESKTOP-ACMQRLH>
2023-11-29 23:24:00 +0200

    Добавьте файлы проекта.

---
+++ Lab.5.1.medium.lvl.kruchinina/Program.cs
@@ -0,0 +1,21 @@
+﻿// 10  var
+Console.Write("Введите символы(без пробелов): ");
+char[] ch = Console.ReadLine().ToCharArray();
+Console.WriteLine("Различные символы в массиве:");
+for (int i = 0; i < ch.Length; i++)
+{
+    char ch1 = ch[i];
+    bool f = true;
+    for (int j = 0; j < i; j++)
+    {
+        if (ch[j] == ch1)
+        {
+            f = false;
+            break;
+        }
+    }
+    if (f)
+    {
+        Console.WriteLine(ch1);
+    }
+}
